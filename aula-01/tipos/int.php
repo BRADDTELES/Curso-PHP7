29a5612318921421f9cd6bffa681d44c74aeb335
BRADDTELES <braddteles@gmail.com>
2024-06-04 06:33:55 -0300

    Tipos Int e Float

---
+++ aula-01/tipos/int.php
@@ -0,0 +1,16 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+
+echo 11;
+echo '<br>';
+
+var_dump(11);
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+echo 017, '<br>'; // Base octal
+echo 0b11000111001, '<br>'; // Base binária
+echo 0x1170cf0, '<br>'; // Base hexa
+
