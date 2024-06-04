54dd7683d5dd253cc5fbb181a4e9e73a9be5b612
BRADDTELES <braddteles@gmail.com>
2024-06-04 07:10:20 -0300

    Desafio Procedência

---
+++ aula-01/tipos/desafio_procedencia.php
@@ -0,0 +1,12 @@
+<div class="titulo">Desafio Procedência</div>
+
+<?php
+echo '<p>1. Qual o valor será impresso?</p>';
+echo 2 + 5 * 3 + (12 / 6) / (-8 + 2 ** 3);
+echo '<p>0</p>';
+echo '<p>2. Qual expressão imprime o valor 100?</p>';
+echo 'a)', (1 + 2) * 20 - 15;
+echo 'b)', 4 * 5 ** 2;
+echo 'c)', 2 **3 ** 4 /1e25;
+echo 'd)', 3 + (3 * 8) / 2 - 3;
+echo '<p>Letra B</p>';
