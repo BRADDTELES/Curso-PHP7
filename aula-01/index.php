c17a61e2d07e1d39452d0edbf4ecb3bce3ed9876
BRADDTELES <braddteles@gmail.com>
2024-06-08 10:56:17 -0300

    Listando o If Else

--- aula-01/index.php
+++ aula-01/index.php
@@ -140,6 +140,16 @@
               </li>
             </ul>
           </div>
+          <div class="modulo roxo">
+            <h3>Controle</h3>
+            <ul>
+              <li>
+                <a href="exercicio.php?dir=controle&file=if_else">
+                  If Else
+                </a>
+              </li>
+            </ul>
+          </div>
         </nav>
       </div>
     </main>
