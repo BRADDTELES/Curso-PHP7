cbc421289bc632b6dd51ac252d5666295c1bd123
BRADDTELES <braddteles@gmail.com>
2024-06-08 09:58:12 -0300

    Listando, o Valor vs Referência das atribuições

--- aula-01/index.php
+++ aula-01/index.php
@@ -128,6 +128,11 @@
                   Desafio Variáveis
                 </a>
               </li>
+              <li>
+                <a href="exercicio.php?dir=variaveis&file=valor_referencia">
+                  Valor vs Referência
+                </a>
+              </li>
             </ul>
           </div>
         </nav>
