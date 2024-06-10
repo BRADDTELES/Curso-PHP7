5ceadfb72e6210d618f708b5ff6d3f608dc5acca
BRADDTELES <braddteles@gmail.com>
2024-06-10 16:25:42 -0300

    Desafio Op. Lógico

--- aula-01/index.php
+++ aula-01/index.php
@@ -43,6 +43,11 @@
                   Operadores Lógicos
                 </a>
               </li>
+              <li>
+                <a href="exercicio.php?dir=controle&file=desafio_operadores_logicos">
+                  Desafio Operadores Lógicos
+                </a>
+              </li>
             </ul>
           </div>
         <div class="modulo azul">
