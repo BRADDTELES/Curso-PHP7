c985d64442abb0a850ce04f914263ff86fe9695d
BRADDTELES <braddteles@gmail.com>
2024-06-10 10:32:53 -0300

    Estilizando Op. Relacionais

--- aula-01/controle/operadores_relacionais.php
+++ aula-01/controle/operadores_relacionais.php
@@ -24,26 +24,14 @@ if($idade < 18) {
     echo "Terceira idade = $idade anos!";
 }
 
-echo '<p class="divisao">Spaceship</p><hr>';
+echo "<p class='divisao'>Spaceship</p><hr>";
 var_dump(500 <=> 3);
 var_dump(50 <=> 50);
 var_dump(5 <=> 50);
 
 
-echo '<p class="divisao">Op. de Negação: Valores pode ser V ou F</p><hr>';
+echo "<p class='divisao'>Op. de Negação: Valores pode ser V ou F</p><hr>";
 var_dump(!!5);
 var_dump(!!0);
 var_dump(!!"");
 var_dump(!!" ");
-
-?>
-
-<style>
-    .divisao {
-        margin-bottom: 0px;
-    }
-
-    hr {
-        margin-top: 0px;
-    }
-</style>
