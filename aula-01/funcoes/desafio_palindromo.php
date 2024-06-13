d891760f50e76b8fa7eec19e58e77f65a92c62ac
BRADDTELES <braddteles@gmail.com>
2024-06-13 13:35:57 -0300

    Desafio Polindromo fim

--- aula-01/funcoes/desafio_palindromo.php
+++ aula-01/funcoes/desafio_palindromo.php
@@ -1,20 +1,38 @@
 <div class="titulo">Desafio Palindromo</div>
 
 <?php
-function palindrome($string){ 
+function palindromoTeste($string){ 
     if (strrev($string) == $string){ 
-        return 1; 
+        return "Palindromo"; 
     }
     else{
-        return 0;
+        return "Não é Palindromo";
     }
 } 
+echo palindromoTeste("Arara") . "<br>";
 
-$palavra = "arara";
-if(palindrome($palavra)){ 
-    echo "Palindrome"; 
-} else { 
-    echo "Not a Palindrome"; 
+function palindromo($palavra){
+    $ultimoIndice = strlen($palavra) - 1;
+    for($i = 0; $i <= $ultimoIndice; $i++){
+        if($palavra[$i] !== $palavra[$ultimoIndice - $i]){
+            return "Não";
+        }
+    }
+    return "Sim";
 }
 
-?> 
+echo palindromo("arara") . " ";
+echo palindromo("ana") , " ";
+echo palindromo("abccba") . " ";
+echo palindromo("bola") , " ";
+
+echo '<br>';
+
+function palidromoSimples($palavra){
+    return $palavra === strrev($palavra) ? "Sim" : "Não";
+}
+
+echo palidromoSimples("arara") . " ";
+echo palidromoSimples("ana") , " ";
+echo palidromoSimples("abccba") . " ";
+echo palidromoSimples("bola") , " ";
