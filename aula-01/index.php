7ff3083f791c8fb031319ce8614a2ad580666536
BRADDTELES <braddteles@gmail.com>
2024-06-14 07:25:00 -0300

    Primeira Classe

--- aula-01/index.php
+++ aula-01/index.php
@@ -20,6 +20,16 @@
     <main class="principal">
       <div class="conteudo">
         <nav class="modulos">
+        <div class="modulo azul-escuro">
+            <h3>8. Paradigma O.O.</h3>
+            <ul>
+              <li>
+                <a href="exercicio.php?dir=classes_objetos&file=classe">
+                  Primeira Classe
+                </a>
+              </li>
+            </ul>
+          </div>
         <div class="modulo vermelho-escuro">
             <h3>7. Funções</h3>
             <ul>
