3f824477db6ffe8a09ca0889b044c542f05c5ee0
BRADDTELES <braddteles@gmail.com>
2024-06-14 00:19:01 -0300

    Map & Filter (rever novamente)

--- aula-01/index.php
+++ aula-01/index.php
@@ -78,6 +78,11 @@
                   Retornando Função
                 </a>
               </li>
+              <li>
+                <a href="exercicio.php?dir=funcoes&file=map_filter">
+                  Map & Filter
+                </a>
+              </li>
             </ul>
           </div>
         <div class="modulo verde-escuro">
