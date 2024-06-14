fd4fbee27fa49e365aae7af47dac1c80b7e1000b
BRADDTELES <braddteles@gmail.com>
2024-06-14 11:36:56 -0300

    Desafio Data fim

--- aula-01/classes_objetos/desafio_classe_data.php
+++ aula-01/classes_objetos/desafio_classe_data.php
@@ -1,7 +1,7 @@
 <div class="titulo">Desafio Classe Data</div>
 
 <?php
-class Data{
+class MinhaData{
     public $dia = 1;
     public $mes = 1;
     public $ano = 1970;
@@ -11,7 +11,32 @@ class Data{
     }
 }
 
-$data1 = new Data();
+$data1 = new MinhaData();
 echo $data1->dia . '/';
 echo $data1->mes . '/';
 echo $data1->ano . '<br>';
+
+echo "Resposta do Professor <br>";
+
+class Data{
+    public $dia = 1;
+    public $mes = 1;
+    public $ano = 1970;
+
+    public function apresentar(){
+        return "{$this->dia}/{$this->mes}/{$this->ano}";
+    }
+}
+
+$aniversario = new Data();
+$aniversario->dia = 15;
+$aniversario->mes = 8;
+$aniversario->ano = 2014;
+
+$casamento = new Data();
+$casamento->dia = 1;
+$casamento->mes = 8;
+$casamento->ano = 2009;
+
+echo $aniversario->apresentar(),"<br>";
+echo $casamento->apresentar();
