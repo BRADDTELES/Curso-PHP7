aaa25f3624a196158a617a2d8a3097a13ffc1576
BRADDTELES <braddteles@gmail.com>
2024-06-18 16:14:08 -0300

    Incluindo o require_once no index e criando o arquivo menu.php, para puxar o menu para o index

---
+++ aula-01/menu.php
@@ -0,0 +1,452 @@
+<nav class="modulos">
+    <div class="modulo roxo-escuro">
+        <h3>9. Includes</h3>
+        <ul>
+            <li>
+                <a class="plain" href="exercicio.php?dir=includes&file=include">
+                    Include
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=includes&file=include_function">
+                    Include em Função
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=includes&file=include_require">
+                    Include vs Require
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=includes&file=require_return">
+                    Require & Return
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=includes&file=include_once">
+                    Include Once
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=includes&file=require_once">
+                    Require Once
+                </a>
+            <li>
+                <a class="plain" href="exercicio.php?dir=includes&file=desafio_include_require">
+                    Desafio Include & Require
+                </a>
+            </li>
+            </li>
+        </ul>
+    </div>
+    <div class="modulo azul-escuro">
+        <h3>8. Paradigma O.O.</h3>
+        <ul>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=classe">
+                    Primeira Classe
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=desafio_classe_data">
+                    Desafio Classe Data
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=construtor_destrutor">
+                    Construtor & Destrutor
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=heranca">
+                    Herança
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=visibilidade">
+                    Modificadores de Acesso
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=static">
+                    Membros Estáticos
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=interface">
+                    Interface
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=abstract">
+                    Classe Abstrata
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=desafio_erros">
+                    Desafio Erros
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=modificador_final">
+                    Modificador Final
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=traits_01">
+                    Traits #01
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=traits_02">
+                    Traits #02
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=metodos_magicos">
+                    Métodos Mágicos
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=classes_objetos&file=polimorfismo">
+                    Polimorfismo
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="modulo vermelho-escuro">
+        <h3>7. Funções</h3>
+        <ul>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=escopo">
+                    Funções & Escopo
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=args_retorno">
+                    Argumentos & Retorno
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=desafio_palindromo">
+                    Desafio Palindromo
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=args_variaveis">
+                    Argumentos Variáveis
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=args_padrao">
+                    Argumentos Padrão
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=usando_tipos">
+                    Usando Tipos
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=anonimas">
+                    Funções Anônimas
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=closure_callable">
+                    Closure & Callable
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=recursividade">
+                    Recursividade
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=desafio_recursivo">
+                    Desafio Recursivo
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=retornando_funcao">
+                    Retornando Função
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=funcoes&file=map_filter">
+                    Map & Filter
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="modulo verde-escuro">
+        <h3>6. Repetições</h3>
+        <ul>
+            <li>
+                <a class="plain" href="exercicio.php?dir=repeticoes&file=for">
+                    Laço For
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=repeticoes&file=desafio_for">
+                    Desafio For
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=repeticoes&file=foreach">
+                    Foreach
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=repeticoes&file=break-continue">
+                    Break/Continue
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=repeticoes&file=desafio_impressao">
+                    Desafio Impressão
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=repeticoes&file=while-doWhile">
+                    While/Do While
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=repeticoes&file=desafio_tabela">
+                    Desafio Tabela #01
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=repeticoes&file=desafio_tabela_2">
+                    Desafio Tabela #02
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="modulo laranja">
+        <h3>5. Array</h3>
+        <ul>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=basico">
+                    Array
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=mapa">
+                    Mapa
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=desafio_indice">
+                    Desafio Índice
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=desafio_meses">
+                    Desafio Meses
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=operacoes">
+                    Operações
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=desafio_sorteio">
+                    Desafio Sorteio
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=array_multidimensionais">
+                    Arrays Multidimensionais
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=array_constantes">
+                    Arrays Constantes
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=get">
+                    $_GET
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=post">
+                    $_POST
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=array&file=comparacao_arrays">
+                    Comparação Arrays
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="modulo roxo">
+        <h3>4. Controle</h3>
+        <ul>
+            <li>
+                <a class="plain" href="exercicio.php?dir=controle&file=if_else">
+                    If Else
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=controle&file=operadores_relacionais">
+                    Op. Relacionais
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=controle&file=desafio_pi">
+                    Desafio PI
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=controle&file=operadores_logicos">
+                    Operadores Lógicos
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=controle&file=desafio_operadores_logicos">
+                    Desafio Operadores Lógicos
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=controle&file=operador_ternario">
+                    Operador Ternário
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=controle&file=switch">
+                    Switch
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=controle&file=desafio_switch">
+                    Desafio Switch
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="modulo azul">
+        <h3>3. Variáveis</h3>
+        <ul>
+            <li>
+                <a class="plain" href="exercicio.php?dir=variaveis&file=basico">
+                    Variáveis
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=variaveis&file=desafio_equacao">
+                    Desafio Equação
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=variaveis&file=atribuicoes">
+                    Atribuições
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=variaveis&file=interpolacao">
+                    Interpolação
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=variaveis&file=variaveis-variaveis">
+                    Variáveis Variáveis
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=variaveis&file=desafio_variaveis">
+                    Desafio Variáveis
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=variaveis&file=valor_referencia">
+                    Valor vs Referência
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=variaveis&file=constantes">
+                    Constantes
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="modulo vermelho">
+        <h3>2. Tipos</h3>
+        <ul>
+            <li>
+                <a class="plain" href="exercicio.php?dir=tipos&file=int">
+                    Tipo Inteiro
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=tipos&file=float">
+                    Tipo Float
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=tipos&file=aritmeticas">
+                    Op. Aritméticas
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=tipos&file=desafio_procedencia">
+                    Desafio Procedência
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=tipos&file=string">
+                    Tipo String
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=tipos&file=desafio_string">
+                    Desafio String
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=tipos&file=booleano">
+                    Tipo Booleano
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=tipos&file=conversoes">
+                    Conversões
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="modulo verde">
+        <h3>1. Básico</h3>
+        <ul>
+            <li>
+                <a class="plain" href="exercicio.php?dir=basico&file=ola">
+                    Ola PHP
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=basico&file=html">
+                    Integração HTML
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=basico&file=css">
+                    Integração CSS
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=basico&file=comentarios">
+                    Comentários PHP
+                </a>
+            </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=basico&file=desafio">
+                    Desafio
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
