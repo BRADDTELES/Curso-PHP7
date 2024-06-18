d432db8eef5eb84c0e3edd7e8687cda098ea14c2
BRADDTELES <braddteles@gmail.com>
2024-06-18 16:28:55 -0300

    Refinando o Desafio do Módulo

--- aula-01/includes/usuario.php
+++ aula-01/includes/usuario.php
@@ -6,20 +6,11 @@ class Usuario extends Pessoa{
 
     function __construct($nome, $idade, $login){
         parent::__construct($nome, $idade);
-        // $this->nome = $nome; // Pode ser utilizado, melhor forma
-        // $this->idade = $idade; // Pode ser utilizado, melhor forma
         $this->login = $login;
-        echo "Usuário criado!<br>";
-    }
-
-    function __destruct(){
-        echo "Usuário diz: Tchau!!<br>";
-        parent::__destruct();
     }
 
     public function apresentar(){
-        // echo "@{$this->login}: {$this->nome}, {$this->idade} anos<br>"; // Pode utilizar dessa forma
-        echo "@{$this->login}: ";
-        parent::apresentar(); // Pode utilizar dessa forma
+        $login = "@{$this->login}: ";
+        return $login . parent::apresentar();
     }
 }
