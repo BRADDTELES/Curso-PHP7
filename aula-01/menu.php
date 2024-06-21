d30b1d63746471755bd8adac25b1a82631f03b09
BRADDTELES <braddteles@gmail.com>
2024-06-21 20:45:50 -0300

    Datas #01, formatando datas

--- aula-01/menu.php
+++ aula-01/menu.php
@@ -1,4 +1,14 @@
 <nav class="modulos">
+<div class="modulo azul">
+        <h3>13. API</h3>
+        <ul>
+            <li>
+                <a class="plain" href="exercicio.php?dir=api&file=datas_01">
+                    Datas #01
+                </a>
+            </li>
+        </ul>
+    </div>
 <div class="modulo vermelho">
         <h3>12. Sessão</h3>
         <ul>
