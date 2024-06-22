80fbb13ca6f55789592d23c507d2031d0149cf1a
BRADDTELES <braddteles@gmail.com>
2024-06-22 11:50:05 -0300

    Upload de Arquivos (muito bom)

--- aula-01/menu.php
+++ aula-01/menu.php
@@ -22,6 +22,11 @@
                     Ler Arquivo
                 </a>
             </li>
+            <li>
+                <a class="plain" href="exercicio.php?dir=api&file=upload">
+                    Upload
+                </a>
+            </li>
         </ul>
     </div>
 <div class="modulo vermelho">
