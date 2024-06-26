aec187418294512fd32d20aa68f55a9ffda4001a
BRADDTELES <braddteles@gmail.com>
2024-06-26 16:32:03 -0300

    Incluindo arquivos css e a inicializando a pasta Views

---
+++ innout-project/src/views/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>In 'n Out</title>
+</head>
+<body>
+    
+</body>
+</html>
