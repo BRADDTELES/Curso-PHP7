6fb4d75bd5c015b8a18d11c57b40be99500ced4a
BRADDTELES <braddteles@gmail.com>
2024-06-26 17:43:20 -0300

    Tela de Autenticação, inserindo a tela de Login e estilizando usando o Bootstrap

--- innout-project/public/index.php
+++ innout-project/public/index.php
@@ -1,9 +1,4 @@
 <?php 
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-foreach (User::get([], 'name') as $user) {
-    echo $user->name; 
-    echo '<br>';
-}
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
