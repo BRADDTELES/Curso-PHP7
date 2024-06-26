5476652459905167323f59018e332d2bbdd6856f
BRADDTELES <braddteles@gmail.com>
2024-06-26 19:53:48 -0300

    Login #02, inicio

--- innout-project/public/index.php
+++ innout-project/public/index.php
@@ -1,18 +1,6 @@
 <?php 
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-// require_once(VIEW_PATH . '/login.php');
+// require_once(CONTROLLER_PATH . '/loginController.php');
 
-require_once(MODEL_PATH . '/Login.php');
-
-$login = new Login([
-    'email' => 'quico@cod3r.com.br',
-    'password' => 'a'
-]);
-
-try {
-    $login->checkLogin();
-    echo "Deu certo :)";
-} catch (Exception $e) {
-    echo "E-mail ou Senha inválido";
-}
+loadView('login', ['texto' => 'abc123']);
--- innout-project/src/views/login.php
+++ innout-project/src/views/login.php
@@ -34,5 +34,6 @@
             </div>
         </div>
     </form>
+    <?= $texto ?>
 </body>
 </html>
