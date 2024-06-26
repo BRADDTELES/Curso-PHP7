35005b0150e33d969214cccf8015b3308217a181
BRADDTELES <braddteles@gmail.com>
2024-06-26 08:40:11 -0300

    Classe User #02, métodos mágicos do get e set

--- innout-project/public/index.php
+++ innout-project/public/index.php
@@ -5,4 +5,5 @@ require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
 print_r($user);
-echo 'Fim!';
+$user->email = 'lucas_alterado@cod3r.com.br';
+print_r($user->email);
