067dd12485b0361b112467020a9a7dc7be6e5990
BRADDTELES <braddteles@gmail.com>
2024-06-27 17:21:38 -0300

    Redirecionamento #01, não finalizado, problema ao sair da pág. de login para o day_records.php na máquina linux usando a extensão PHP Server no VsCode, ou seja, não estou usando o MAMP

--- innout-project/public/index.php
+++ innout-project/public/index.php
@@ -1,4 +1,10 @@
 <?php 
-
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(CONTROLLER_PATH . '/loginController.php');
+// require_once(CONTROLLER_PATH . '/loginController.php');
+$uri = urldecode($_SERVER['REQUEST_URI']);
+
+if($uri === '/' || $uri === '' ||  $uri === '/index.php' ||  $uri == '/innout-project/public/index.php') {
+    $uri = '/loginController.php';
+}
+
+require_once(CONTROLLER_PATH . "/{$uri}");
--- innout-project/src/controllers/loginController.php
+++ innout-project/src/controllers/loginController.php
@@ -1,13 +1,13 @@
 <?php
 loadModel('Login');
+
 $exception = null;
 
 if(count($_POST) > 0) {
-    $login  = new Login($_POST);
+    $login = new Login($_POST);
     try {
         $user = $login->checkLogin();
-        // echo "Usuário {$user->name} logado :)";
-        throw new AppException("Usuário {$user->name} logado");
+        header("Location: day_records.php");
     } catch (AppException $e) {
         $exception = $e;
     }
