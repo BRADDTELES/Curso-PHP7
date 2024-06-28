51ffc0e4a50c0e8be9be35ded39fdccc3cf15941
BRADDTELES <braddteles@gmail.com>
2024-06-27 23:17:33 -0300

    up index.php

--- innout-project/public/index.php
+++ innout-project/public/index.php
@@ -1,9 +1,9 @@
 <?php 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 // require_once(CONTROLLER_PATH . '/loginController.php');
-$uri = urldecode($_SERVER['REQUEST_URI']);
-
-if($uri === '/' || $uri === '' ||  $uri === '/index.php' ||  $uri == '/innout-project/public/index.php' || $uri == '\\' || $uri == '//innout-project//public//index.php') {
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+// $uri = urldecode($_SERVER['REQUEST_URI']);
+if($uri === '/' || $uri === '' ||  $uri === '/index.php' || $uri == '/innout-project/public/index.php') {
     $uri = '/loginController.php';
 }
 
