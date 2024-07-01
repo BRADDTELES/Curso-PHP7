0b5c9a8fcd28529476ad1d81761b82bb292b0365
BRADDTELES <braddteles@gmail.com>
2024-07-01 06:52:19 -0300

    CSS Template #04, finalizando o layout do Sair

--- innout-project/src/views/template/header.php
+++ innout-project/src/views/template/header.php
@@ -34,7 +34,7 @@
                 <ul class="nav-list">
                     <li class="nav-item">
                         <a href="logout.php">
-                            <i class="iconfont-logout mr-2">Sair</i>
+                            <i class="icofont-logout mr-2">Sair</i>
                         </a>
                     </li>
                 </ul>
