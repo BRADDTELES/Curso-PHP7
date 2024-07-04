96a8fe7e998a30ea8175028614e08903a35968a3
BRADDTELES <braddteles@gmail.com>
2024-07-03 22:08:27 -0300

    Bater Ponto #01, codificação complicaddinha, as vezes dá erro

--- innout-project/src/controllers/day_records.php
+++ innout-project/src/controllers/day_records.php
@@ -5,10 +5,10 @@ requireValidSession();
 loadModel('WorkingHours');
 
 $date = (new DateTime())->getTimestamp();
-$today = strftime("%d de %B de %Y", $date); // <--- %d de %B de %Y
+$today = strftime('%d de %B de %Y', $date); // <--- %d de %B de %Y
 
 $user = $_SESSION['user'];
-$records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d')); 
+$records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 
 loadTemplateView('day_records', [
     'today' => $today,
