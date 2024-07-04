373da8da3b7cb2e22bf450dab8d7813e12ff00bc
BRADDTELES <braddteles@gmail.com>
2024-07-04 06:18:28 -0300

    Bater Ponto Forçado

--- innout-project/src/controllers/innout.php
+++ innout-project/src/controllers/innout.php
@@ -9,6 +9,11 @@ $records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 
 try{
     $currentTime = strftime('%H:%M:%S', time());
+
+    if($_POST['forcedTime']) {
+        $currentTime = $_POST['forcedTime'];
+    }
+
     $records->innout($currentTime);
     addSuccessMsg('Ponto inserido com sucesso!');
 } catch(AppException $e) {
