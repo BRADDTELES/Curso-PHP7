05beacc6e80badc6a328e3358a02edd06838e53f
BRADDTELES <braddteles@gmail.com>
2024-07-03 21:07:06 -0300

    Visualizar Batimentos

--- innout-project/src/controllers/day_records.php
+++ innout-project/src/controllers/day_records.php
@@ -2,7 +2,15 @@
 session_start();
 requireValidSession();
 
+loadModel('WorkingHours');
+
 $date = (new DateTime())->getTimestamp();
 $today = strftime("%d de %B de %Y", $date); // <--- %d de %B de %Y
 
-loadTemplateView('day_records', ['today' => $today]);
+$user = $_SESSION['user'];
+$records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d')); 
+
+loadTemplateView('day_records', [
+    'today' => $today,
+    'records' => $records
+]);
