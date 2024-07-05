bdf6a527a29d6329b92aa9153327492306fd305e
BRADDTELES <braddteles@gmail.com>
2024-07-04 21:14:30 -0300

    Calcular Intervalo do Almoço

--- innout-project/src/controllers/test.php
+++ innout-project/src/controllers/test.php
@@ -4,7 +4,11 @@
 loadModel('WorkingHours');
 
 $wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
+
+$workedIntervalString = $wh->getWorkedInterval()->format('%H:%I:%S');
+print_r($workedIntervalString);
 echo '<br>';
-$workedInterval = $wh->getWorkedInterval()->format('%H:%I:%S');
-print_r($workedInterval);
+
+$lunchIntervalString = $wh->getLunchInterval()->format('%H:%I:%S');
+print_r($lunchIntervalString);
 echo '<br>';
