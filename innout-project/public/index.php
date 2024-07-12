f0d96d791614615a1adeff3614d5bbfb3756512c
BRADDTELES <braddteles@gmail.com>
2024-07-12 20:20:00 -0300

    up.

--- innout-project/public/index.php
+++ innout-project/public/index.php
@@ -1,7 +1,8 @@
 <?php 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-if ($uri === '/' || $uri === '' || $uri === '/index.php') {
+var_dump($uri);
+if ($uri === '/' || $uri === '' || $uri === '/index.php' || $uri === '/Curso-PHP7/innout-project/public/index.php') {
     $uri = '/day_records.php';
 }
 
