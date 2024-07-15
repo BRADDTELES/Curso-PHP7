6296023516278856431b8ca94033ef71e7dc06a3
BRADDTELES <braddteles@gmail.com>
2024-07-15 07:06:14 -0300

    Usando Template Strings

---
+++ exercicios-js/fundamentos/templateString.js
@@ -0,0 +1,15 @@
+const nome = 'Rebeca'
+const concatenacao = 'Olá ' + nome + '!'
+/* Utilizando a Crase
+* em vez das aspas
+*/
+const template = `
+    Olá
+    ${nome}!`
+console.log(concatenacao, template)
+
+// expressoes...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
