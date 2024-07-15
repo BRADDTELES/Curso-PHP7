7569e9469d7f34a7d174d7694183d7685479c300
BRADDTELES <braddteles@gmail.com>
2024-07-15 05:24:23 -0300

    ft. Organização Básica de código JS

--- exercicios-js/fundamentos/organizacao.js
+++ exercicios-js/fundamentos/organizacao.js
@@ -1,4 +1,8 @@
-// JavaScript é organizado em Sentença de código, que podem ou não terminar com ponto e vírgula, também é organizado em Bloco de código, que são delimitado por chaves
+/* JavaScript é organizado em Sentença de código,
+* que podem ou não terminar com ponto e vírgula,
+* também é organizado em Bloco de código,
+* que são delimitado por chaves.
+*/
 console.log("Sentença de código")
 
 {
