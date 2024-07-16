05f0060e2bf6252b28a1d1fb28f0b652bbf2f674
BRADDTELES <braddteles@gmail.com>
2024-07-16 05:29:17 -0300

    Operadores: Atribuição

---
+++ exercicios-js/fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7
+let b = 3
+
+b += a // b = b + a
+console.log(b)
+
+b -= 4 // b = b(10) - 4
+console.log(b)
+
+b *= 2 // b = b(6) * 2
+console.log(b)
+
+b /= 2 // b = b(12) / 2
+console.log(b)
+
+b %= 2 // b = b(6) % 2
+console.log(b)
