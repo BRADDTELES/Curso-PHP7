e6915a969375984e0f24d38d85bfc010c8217d83
BRADDTELES <braddteles@gmail.com>
2024-08-28 16:21:59 -0300

    Inserir Cidades

---
+++ exercicios-web/banco-relacional/inserirCidades.sql
@@ -0,0 +1,27 @@
+select * from estados where sigla = 'SP'
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Campinas', 795, 26)
+
+select * from estados where sigla = 'RJ'
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Niterói', 133.9, 20)
+
+INSERT INTO cidades 
+    (nome, area, estado_id)
+VALUES (
+    'Caruaru', 
+    920.6, 
+    (select id from estados where sigla = 'PE')
+)
+
+INSERT INTO cidades 
+    (nome, area, estado_id)
+VALUES (
+    'Juazeiro do Norte', 
+    248.2, 
+    (select id from estados where sigla = 'CE')
+)
+
+select * from cidades
